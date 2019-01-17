1ba9b4fb989b103a3852442589ae76103ae8153f
EC2 Default User <ec2-user@ip-172-31-23-84.ec2.internal>
2019-01-17 17:30:12 +0000

    first commit

---
+++ application/Demo/lib/cosmos/version.rb
@@ -0,0 +1,12 @@
+# encoding: ascii-8bit
+
+COSMOS_VERSION = '4.3.0'
+module Cosmos
+  module Version
+    MAJOR = '4'
+    MINOR = '3'
+    PATCH = '0'
+    BUILD = '3588276d89cb9d934e0e51ad8c6e2426fc5dafb5'
+  end
+  VERSION = '4.3.0'
+end
